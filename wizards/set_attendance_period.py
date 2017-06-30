04df299564e38331cb15d40065555010db2df862
saul <saul.infobit@gmail.com>
2017-06-30 17:56:59 +0200

    [UPDATE]update module to 10.0

--- wizards/set_attendance_period.py
+++ wizards/set_attendance_period.py
@@ -10,39 +10,29 @@ class attendanceReportWizard(models.TransientModel):
 		string='Employee'
 		)
 
-	def print_report(self,cr,uid,ids,context=None):
-	    attendance = self.browse(cr,uid,ids,context=context)
+	def print_report(self):
+	    attendance = self
 	    datas = {}
             if attendance.date_start >= attendance.date_end:
 	    	raise exceptions.Warning(_('Warning!'),_('End date is %s must be greater then start date is %s') % (attendance.date_start,attendance.date_end))
 	    #search employee services
-	    services_ids = self.pool.get("hr.attendance").search_read(cr,uid,[('employee_id','=',attendance.employee_id.id),('name','>=',attendance.date_start),('name','<=',attendance.date_end)],context=context)
-	    services_ids.reverse() 
+	    services_ids = self.env["hr.attendance"].search_read([('employee_id','=',attendance.employee_id.id),('check_in','>=',attendance.date_start),('check_out','<=',attendance.date_end)],order='check_in asc')
+	    #raise exceptions.Warning(services_ids)
+	    #services_ids.reverse()
 	    #raise exceptions.Warning(services_ids[0]['display_name'])
 	    if not services_ids:
 		raise exceptions.Warning(_('Warning!'),_('The selected employee has no record in the selected period'))
-	    #res= {'date_start':activo.date_start,'date_end':activo.date_end}
-            res = self.read(cr, uid, ids)[0]
+	    res= {'date_start':self.date_start,'date_end':self.date_end}
+	    #raise exceptions.Warning(self)
+            #res = self
 	    #raise exceptions.Warning(res)
 	    if services_ids:
 	            data = res
 	            datas = {
 		            'attendances': services_ids,
-#		            'model': 'account.report.active', # wizard model name
 			    'form':data,
-#		            'context':context
 	            }
-#	    raise exceptions.Warning(datas['form'][0]['date_start'])
-	    #raise exceptions.Warning(datas)
-	    
-	    return self.pool['report'].get_action(cr, uid, [],'ib_report_attendance_w.report_attendance_period_document', data=datas, context=context)
-
-#            return {
-#                   'type': 'ir.actions.report.xml',
-#                   'report_name': 'ib_report_activos.report_activo_grupo_document',#module name.report template name
-#                   'data': datas,
-#               }	   
-
+	    return self.env['report'].get_action(self,'ib_report_attendance_w.report_attendance_period_document', data=datas)
 
 	@api.model
 	def default_get(self, fields):
