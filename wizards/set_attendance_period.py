8ad28d50505b0638602c624b23ba90bc200e6e84
saul <saul.infobit@gmail.com>
2017-07-03 11:05:06 +0200

    [FIX]time utc to 'Europe/Madrid'

--- wizards/set_attendance_period.py
+++ wizards/set_attendance_period.py
@@ -1,8 +1,11 @@
 from openerp import models, fields, api, exceptions, _
+import pytz, datetime
+from pytz import timezone
+from datetime import datetime
+
 
 class attendanceReportWizard(models.TransientModel):
 	_name = 'attendance.set.period'
-	#_inherit = 'hr.attendance'
 	date_start = fields.Date('Start Date')
 	date_end = fields.Date('End Date')
 	employee_id = fields.Many2one(
@@ -17,15 +20,20 @@ class attendanceReportWizard(models.TransientModel):
 	    	raise exceptions.Warning(_('Warning!'),_('End date is %s must be greater then start date is %s') % (attendance.date_start,attendance.date_end))
 	    #search employee services
 	    services_ids = self.env["hr.attendance"].search_read([('employee_id','=',attendance.employee_id.id),('check_in','>=',attendance.date_start),('check_out','<=',attendance.date_end)],order='check_in asc')
-	    #raise exceptions.Warning(services_ids)
-	    #services_ids.reverse()
-	    #raise exceptions.Warning(services_ids[0]['display_name'])
+	    #UTC to local time
+	    local_tz = pytz.timezone('Europe/Madrid')
+
+	    for service in services_ids:
+		    check_in = datetime.strptime(service['check_in'],"%Y-%m-%d %H:%M:%S")
+		    check_out = datetime.strptime(service['check_out'],"%Y-%m-%d %H:%M:%S")
+		    local_time_in = check_in.replace(tzinfo=pytz.utc).astimezone(local_tz)
+		    local_time_out = check_out.replace(tzinfo=pytz.utc).astimezone(local_tz)
+		    service['check_in']= local_time_in.strftime("%Y-%m-%d %H:%M:%S")
+		    service['check_out'] = local_time_out.strftime("%Y-%m-%d %H:%M:%S")
+
 	    if not services_ids:
 		raise exceptions.Warning(_('Warning!'),_('The selected employee has no record in the selected period'))
 	    res= {'date_start':self.date_start,'date_end':self.date_end}
-	    #raise exceptions.Warning(self)
-            #res = self
-	    #raise exceptions.Warning(res)
 	    if services_ids:
 	            data = res
 	            datas = {
